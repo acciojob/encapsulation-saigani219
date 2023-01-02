694da325cef0355b9dcb21a911cc1e14dab9fdb6
sai ganesh <saigani1002@gmail.com>
2023-01-02 22:10:09 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+	  RWOnly rwOnly = new RWOnly();
+	  rwOnly.setName("Ganesh");
+	  System.out.println(rwOnly.getName());
+	  //name has private access in com.driver.RWOnly
+  }
 }
